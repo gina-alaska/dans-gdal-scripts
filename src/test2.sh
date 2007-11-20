8479b94c0699a488d2596c271d6c7cf8d9300e30
Dan Stahlke <dan@gina.alaska.edu>
2007-11-20 03:23:13 +0000

    added sql test

--- src/test2.sh
+++ src/test2.sh
@@ -1,1 +1,2 @@
-./gdal_list_corners testcase2.tif -ll_en 0 0 -res 1 -inspect-contour -nodataval 255 -dp-toler .3 -skip-erosion -wkt-en zz.wkt -wkt-split-polys
+./gdal_list_corners testcase2.tif -ll_en 0 0 -res 1 -inspect-contour -nodataval 255 -dp-toler .3 -skip-erosion -wkt-en zz.wkt
+( echo -n "select isvalid(mpolyfromtext('" ; cat zz.wkt ; echo -n "'));" ) |psql sv_ion
