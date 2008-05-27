5a2f79f64f443bdac51fd5fe0a604feb5bec071b
Dan Stahlke <dan@gina.alaska.edu>
2008-05-27 23:35:06 +0000

    updated makefile and test case scripts

--- src/tests/test1.sh
+++ src/tests/test1.sh
@@ -1,14 +1,20 @@
-#!/bin/sh -x
-#./gdal_trace_outline testcase.tif  -nodataval 255 -out-cs xy -wkt-out good-tc1.wkt -split-polys -dp-toler 0
-#./gdal_trace_outline testcase2.tif -nodataval 255 -out-cs xy -wkt-out good-tc2.wkt -split-polys -dp-toler 0
-#./gdal_trace_outline testcase3.tif -nodataval 255 -out-cs xy -wkt-out good-tc3.wkt -split-polys -dp-toler 0
-#./gdal_trace_outline testcase4.png -nodataval 255 -out-cs xy -wkt-out good-tc4.wkt -split-polys -dp-toler 0
-#./gdal_trace_outline testcase5.png -nodataval 255 -out-cs xy -wkt-out good-tc5.wkt -split-polys -dp-toler 0
+#!/bin/sh
+#../gdal_trace_outline testcase.tif  -nodataval 255 -out-cs xy -wkt-out good-tc1.wkt -split-polys -dp-toler 0
+#../gdal_trace_outline testcase2.tif -nodataval 255 -out-cs xy -wkt-out good-tc2.wkt -split-polys -dp-toler 0
+#../gdal_trace_outline testcase3.tif -nodataval 255 -out-cs xy -wkt-out good-tc3.wkt -split-polys -dp-toler 0
+#../gdal_trace_outline testcase4.png -nodataval 255 -out-cs xy -wkt-out good-tc4.wkt -split-polys -dp-toler 0
+#../gdal_trace_outline testcase5.png -nodataval 255 -out-cs xy -wkt-out good-tc5.wkt -split-polys -dp-toler 0
 rm -f test-tc*.wkt
-./gdal_trace_outline testcase.tif  -nodataval 255 -out-cs xy -wkt-out test-tc1.wkt -split-polys -dp-toler 0
-./gdal_trace_outline testcase2.tif -nodataval 255 -out-cs xy -wkt-out test-tc2.wkt -split-polys -dp-toler 0
-./gdal_trace_outline testcase3.tif -nodataval 255 -out-cs xy -wkt-out test-tc3.wkt -split-polys -dp-toler 0
-./gdal_trace_outline testcase4.png -nodataval 255 -out-cs xy -wkt-out test-tc4.wkt -split-polys -dp-toler 0
-./gdal_trace_outline testcase5.png -nodataval 255 -out-cs xy -wkt-out test-tc5.wkt -split-polys -dp-toler 0
-for i in 1 2 3 4 5 ; do md5sum good-tc$i.wkt test-tc$i.wkt ; done
-for i in 1 2 3 4 5 ; do diff --brief good-tc$i.wkt test-tc$i.wkt ; done
+../gdal_trace_outline testcase.tif  -nodataval 255 -out-cs xy -wkt-out test-tc1.wkt -split-polys -dp-toler 0
+../gdal_trace_outline testcase2.tif -nodataval 255 -out-cs xy -wkt-out test-tc2.wkt -split-polys -dp-toler 0
+../gdal_trace_outline testcase3.tif -nodataval 255 -out-cs xy -wkt-out test-tc3.wkt -split-polys -dp-toler 0
+../gdal_trace_outline testcase4.png -nodataval 255 -out-cs xy -wkt-out test-tc4.wkt -split-polys -dp-toler 0
+../gdal_trace_outline testcase5.png -nodataval 255 -out-cs xy -wkt-out test-tc5.wkt -split-polys -dp-toler 0
+#for i in 1 2 3 4 5 ; do md5sum good-tc$i.wkt test-tc$i.wkt ; done
+for i in 1 2 3 4 5 ; do 
+	if diff --brief good-tc$i.wkt test-tc$i.wkt ; then
+		echo "$i - Good"
+	else
+		echo "$i - Bad"
+	fi
+done
--- src/tests/test2.sh
+++ src/tests/test2.sh
@@ -1,2 +1,3 @@
-./gdal_trace_outline testcase2.tif -nodataval 255 -dp-toler .3 -out-cs xy -wkt-out zz.wkt
+#!/bin/sh
+../gdal_trace_outline testcase2.tif -nodataval 255 -dp-toler .3 -out-cs xy -wkt-out zz.wkt
 ( echo -n "select isvalid(mpolyfromtext('" ; cat zz.wkt ; echo -n "'));" ) |psql sv_ion
