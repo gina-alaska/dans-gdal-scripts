f537a4a10dafeab224c98adbd9cd9ceabb3d66c3
Dan Stahlke <dan@gina.alaska.edu>
2008-06-25 22:30:45 +0000

    antialias and speed

--- src/hough.cc
+++ src/hough.cc
@@ -30,28 +30,44 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // See http://en.wikipedia.org/wiki/Hough_transform
 
+typedef float accum_t;
+
 typedef struct {
-	int w, h;
-	int *accum;
+	int w_ang, w_rad;
+	accum_t *accum;
 	vertex_t center;
 	double *sin_table, *cos_table;
 } hough_accum_t;
 
-static inline void hough_add_point(hough_accum_t *ha, double x0, double y0) {
-	int w = ha->w;
-	int h = ha->h;
-	x0 -= ha->center.x;
-	y0 -= ha->center.y;
-	for(int theta_i=0; theta_i<w; theta_i++) {
+static inline void hough_add_seg(hough_accum_t *ha, vertex_t p1, vertex_t p2) {
+	int w_ang = ha->w_ang;
+	int w_rad = ha->w_rad;
+
+	p1.x -= ha->center.x;
+	p1.y -= ha->center.y;
+	p2.x -= ha->center.x;
+	p2.y -= ha->center.y;
+
+	double dx = p2.x - p1.x;
+	double dy = p2.y - p1.y;
+	double weight = sqrt(dx*dx + dy*dy);
+
+	for(int theta_i=0; theta_i<w_ang; theta_i++) {
+		accum_t *acc = ha->accum + theta_i * w_rad;
 		double sin = ha->sin_table[theta_i];
 		double cos = ha->cos_table[theta_i];
-		double r = x0*cos + y0*sin;
-		int r_i = (int)round(r) + h/2;
-		if(r_i >= 0 && r_i < h) {
-			int p = theta_i + r_i * w;
-			ha->accum[p]++;
+		double r1 = p1.x*cos + p1.y*sin + (double)(w_rad/2);
+		double r2 = p2.x*cos + p2.y*sin + (double)(w_rad/2);
+		if(r2 < r1) { double tmp = r1; r1 = r2; r2 = tmp; } // swap
+		int r1_i = (int)floor(r1);
+		int r2_i = (int)floor(r2);
+		if(r1_i == r2_i) {
+			acc[r1_i] += weight;
 		} else {
-			printf("oob: %d\n", r_i);
+			double w2 = weight / (r2-r1);
+			acc[r1_i] += w2 * ((double)(r1_i+1) - r1);
+			acc[r2_i] += w2 * (r2 - (double)r2_i);
+			for(int i=r1_i+1; i<r2_i; i++) acc[i] += w2;
 		}
 	}
 }
@@ -60,17 +76,7 @@ static void hough_add_ring(hough_accum_t *ha, ring_t *ring) {
 	for(int r_idx=0; r_idx<ring->npts; r_idx++) {
 		vertex_t v1 = ring->pts[r_idx];
 		vertex_t v2 = ring->pts[(r_idx+1) % ring->npts];
-		double dx = v2.x - v1.x;
-		double dy = v2.y - v1.y;
-		double len = sqrt(dx*dx + dy*dy);
-		int nsteps = (int)round(len);
-		for(int s_idx=0; s_idx<nsteps; s_idx++) {
-			double alpha = ((double)s_idx + 0.5) / (double)nsteps;
-			double x = v1.x + dx * alpha;
-			double y = v1.y + dy * alpha;
-			//printf("alpha=%g x=%g y=%g\n", alpha, x, y);
-			hough_add_point(ha, x, y);
-		}
+		hough_add_seg(ha, v1, v2);
 	}
 }
 
@@ -83,15 +89,15 @@ static double color_step(double v) {
 }
 
 static void hough_accum_dump(hough_accum_t *ha, const char *fn) {
-	int w = ha->w;
-	int h = ha->h;
-	int maxval = 0;
-	for(int i=0; i<w*h; i++) {
+	int w_ang = ha->w_ang;
+	int w_rad = ha->w_rad;
+	accum_t maxval = 0;
+	for(int i=0; i<w_ang*w_rad; i++) {
 		if(ha->accum[i] > maxval) maxval = ha->accum[i];
 	}
-	uint8_t *buf = (uint8_t *)malloc_or_die(w*h*3);
+	uint8_t *buf = (uint8_t *)malloc_or_die(w_ang*w_rad*3);
 	float lmv = logf(maxval+1);
-	for(int i=0; i<w*h; i++) {
+	for(int i=0; i<w_ang*w_rad; i++) {
 		float v = logf(ha->accum[i]+1) / lmv;
 		double b = color_step(v-1.0/4.0) + color_step(v-1.0);
 		double g = color_step(v-2.0/4.0) + color_step(v-1.0);
@@ -101,18 +107,18 @@ static void hough_accum_dump(hough_accum_t *ha, const char *fn) {
 		buf[i*3+2] = b>1 ? 255 : (int)(b*255.0);
 	}
 	FILE *fh = fopen(fn, "w");
-	fprintf(fh, "P6\n%d %d\n255\n", w, h);
-	fwrite(buf, 1, w*h*3, fh);
+	fprintf(fh, "P6\n%d %d\n255\n", w_ang, w_rad);
+	fwrite(buf, 1, w_ang*w_rad*3, fh);
 	fclose(fh);
 }
 
 void do_hough(mpoly_t *mp) {
 	printf("hough init\n");
 	hough_accum_t *ha = (hough_accum_t *)malloc_or_die(sizeof(hough_accum_t));
-	ha->w = 5000;
-	ha->h = 5000;
-	ha->accum = (int *)malloc_or_die(sizeof(int) * ha->w * ha->h);
-	memset(ha->accum, 0, sizeof(int) * ha->w * ha->h);
+	ha->w_ang = 5000;
+	ha->w_rad = 5000;
+	ha->accum = (accum_t *)malloc_or_die(sizeof(accum_t) * ha->w_ang * ha->w_rad);
+	for(int i=0; i<ha->w_ang*ha->w_rad; i++) ha->accum[i] = 0;
 
 	bbox_t bbox = get_polygon_bbox(mp);
 	ha->center.x = (bbox.max_x + bbox.min_x) / 2.0;
@@ -120,13 +126,13 @@ void do_hough(mpoly_t *mp) {
 
 	double max_x = (bbox.max_x - bbox.min_x) / 2.0;
 	double max_y = (bbox.max_y - bbox.min_y) / 2.0;
-	double scale_x = (double)(ha->h/2-1) / (max_x*sqrt(2.0));
-	double scale_y = (double)(ha->h/2-1) / (max_y*sqrt(2.0));
-	ha->sin_table = (double *)malloc_or_die(sizeof(double) * ha->w);
-	ha->cos_table = (double *)malloc_or_die(sizeof(double) * ha->w);
-	for(int i=0; i<ha->w; i++) {
+	double scale_x = (double)(ha->w_rad/2-1) / (max_x*sqrt(2.0));
+	double scale_y = (double)(ha->w_rad/2-1) / (max_y*sqrt(2.0));
+	ha->sin_table = (double *)malloc_or_die(sizeof(double) * ha->w_ang);
+	ha->cos_table = (double *)malloc_or_die(sizeof(double) * ha->w_ang);
+	for(int i=0; i<ha->w_ang; i++) {
 		// FIXME - use full wave and take into account handedness of border
-		double theta = (double)i / (double)ha->w * M_PI;
+		double theta = (double)i / (double)ha->w_ang * M_PI;
 		ha->cos_table[i] = cos(theta) * scale_x;
 		ha->sin_table[i] = sin(theta) * scale_y;
 	}
