d6dad194b7030dba70dbad8d56adba780435f85f
Dan Stahlke <dan@gina.alaska.edu>
2008-07-23 22:29:18 +0000

    moved some stuff to the attic

--- src/gdal_trace_outline.cc
+++ src/gdal_trace_outline.cc
@@ -440,7 +440,6 @@ int main(int argc, char **argv) {
 
 		if(feature_poly.num_rings && do_pinch_excursions) {
 			printf("Pinching excursions...\n");
-			//feature_poly = pinch_excursions(&feature_poly, NULL);
 			feature_poly = pinch_excursions2(&feature_poly, dbuf);
 			printf("Done pinching excursions.\n");
 		}
