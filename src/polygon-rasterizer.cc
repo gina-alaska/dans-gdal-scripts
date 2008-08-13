dfa6c3c1cf468e8149cb571484efc111a7541c29
Dan Stahlke <dan@gina.alaska.edu>
2008-08-13 22:28:14 +0000

    only give pixels that are entirely contained within polygon

--- src/polygon-rasterizer.cc
+++ src/polygon-rasterizer.cc
@@ -28,19 +28,68 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "polygon.h"
 #include "polygon-rasterizer.h"
 
-static int intcompare(const void *ap, const void *bp) {
-	int a = *((int *)ap);
-	int b = *((int *)bp);
+typedef struct {
+	int num_crossings;
+	int array_size;
+	double *crossings;
+} row_crossings_dbl_t;
+
+row_crossings_t crossings_dbl_to_int(row_crossings_dbl_t *in) {
+	row_crossings_t out;
+	out.num_crossings = 0;
+	out.array_size = in->num_crossings;
+	if(in->num_crossings == 0) {
+		out.crossings = NULL;
+	} else {
+		out.crossings = (int *)malloc_or_die(sizeof(int) * out.array_size);
+		for(int i=0; i<in->num_crossings; i+=2) {
+			int from = (int)ceil(in->crossings[i]);
+			int to = (int)floor(in->crossings[i+1]);
+			if(to > from) {
+				out.crossings[out.num_crossings++] = from;
+				out.crossings[out.num_crossings++] = to;
+			}
+		}
+	}
+	return out;
+}
+
+static int dbl_compare(const void *ap, const void *bp) {
+	double a = *((double *)ap);
+	double b = *((double *)bp);
 	return (a<b) ? -1 : (a>b) ? 1 : 0;
 }
 
+static void dbl_crossings_sort(row_crossings_dbl_t *r) {
+	double *c = r->crossings;
+	if(r->num_crossings == 2) {
+		if(c[0] > c[1]) {
+			double tmp = c[0];
+			c[0] = c[1];
+			c[1] = tmp;
+		}
+	} else {
+		if(r->num_crossings % 2) {
+			fatal_error("should not have an odd number of crossings");
+		}
+		qsort(c, r->num_crossings, sizeof(double), dbl_compare);
+	}
+}
+
+// This function returns a list of pixel ranges for each row.  The ranges consist of pixels
+// that are entirely contained within the polygon.  The results will be slightly wrong for
+// polygons whose vertices are not integers.
 row_crossings_t *get_row_crossings(mpoly_t *mpoly, int min_y, int num_rows) {
-	row_crossings_t *rows = (row_crossings_t *)malloc_or_die(sizeof(row_crossings_t) * num_rows);
+	row_crossings_dbl_t *rows_top = (row_crossings_dbl_t *)malloc_or_die(sizeof(row_crossings_dbl_t) * num_rows);
+	row_crossings_dbl_t *rows_bot = (row_crossings_dbl_t *)malloc_or_die(sizeof(row_crossings_dbl_t) * num_rows);
 
 	for(int i=0; i<num_rows; i++) {
-		rows[i].num_crossings = 0;
-		rows[i].array_size = 0;
-		rows[i].crossings = NULL;
+		rows_top[i].num_crossings = 0;
+		rows_top[i].array_size = 0;
+		rows_top[i].crossings = NULL;
+		rows_bot[i].num_crossings = 0;
+		rows_bot[i].array_size = 0;
+		rows_bot[i].crossings = NULL;
 	}
 
 	for(int i=0; i<mpoly->num_rings; i++) {
@@ -57,39 +106,69 @@ row_crossings_t *get_row_crossings(mpoly_t *mpoly, int min_y, int num_rows) {
 				tmp=y0; y0=y1; y1=tmp; 
 			}
 			double alpha = (x1-x0) / (y1-y0);
-			for(int y=(int)round(y0); y<(int)round(y1); y++) {
-				int row = y - min_y;
-				if(row<0 || row>num_rows-1) continue;
-				row_crossings_t *r = rows+row;
-				int x = (int)round(x0 + ((double)y - y0)*alpha);
-				if(r->num_crossings == r->array_size) {
-					r->array_size += 16;
-					r->crossings = (int *)realloc_or_die(r->crossings,
-						sizeof(int) * r->array_size);
+			int y0i = (int)round(y0);
+			int y1i = (int)round(y1);
+			for(int y=y0i; y<=y1i; y++) {
+				double x = x0 + ((double)y - y0)*alpha;
+
+				int row = y - min_y - 1;
+				if(y > y0i && row >= 0 && row < num_rows) {
+					row_crossings_dbl_t *r = rows_bot + row;
+					if(r->num_crossings == r->array_size) {
+						r->array_size += 16;
+						r->crossings = (double *)realloc_or_die(r->crossings,
+							sizeof(double) * r->array_size);
+					}
+					r->crossings[r->num_crossings++] = x;
 				}
-				r->crossings[r->num_crossings++] = x;
+
+				row = y - min_y;
+				if(y < y1i && row >= 0 && row < num_rows) {
+					row_crossings_dbl_t *r = rows_top + row;
+					if(r->num_crossings == r->array_size) {
+						r->array_size += 16;
+						r->crossings = (double *)realloc_or_die(r->crossings,
+							sizeof(double) * r->array_size);
+					}
+					r->crossings[r->num_crossings++] = x;
+				}
 			}
 		}
+	}
+
+	row_crossings_t *rows_out = (row_crossings_t *)malloc_or_die(sizeof(row_crossings_t) * num_rows);
+
+	for(int i=0; i<num_rows; i++) {
+		rows_out[i].num_crossings = 0;
+		rows_out[i].array_size = 0;
+		rows_out[i].crossings = NULL;
 	}
 
 	for(int row=0; row<num_rows; row++) {
-		row_crossings_t *r = rows+row;
-		int *c = r->crossings;
-		if(r->num_crossings == 2) {
-			if(c[0] > c[1]) {
-				int tmp = c[0];
-				c[0] = c[1];
-				c[1] = tmp;
-			}
+		dbl_crossings_sort(rows_top + row);
+		dbl_crossings_sort(rows_bot + row);
+		row_crossings_t top = crossings_dbl_to_int(rows_top + row);
+		row_crossings_t bot = crossings_dbl_to_int(rows_bot + row);
+		if(top.num_crossings && bot.num_crossings) {
+			crossings_intersection(rows_out + row, &top, &bot);
+			free(top.crossings);
+			free(bot.crossings);
+		} else if(top.num_crossings) {
+			rows_out[row] = top;
+		} else if(bot.num_crossings) {
+			rows_out[row] = bot;
 		} else {
-			if(r->num_crossings % 2) {
-				fatal_error("should not have an odd number of crossings");
-			}
-			qsort(c, r->num_crossings, sizeof(int), intcompare);
+			rows_out[row].num_crossings = 0;
+			rows_out[row].array_size = 0;
+			rows_out[row].crossings = NULL;
 		}
+		if(rows_top[row].crossings) free(rows_top[row].crossings);
+		if(rows_bot[row].crossings) free(rows_bot[row].crossings);
 	}
+	free(rows_top);
+	free(rows_bot);
 
-	return rows;
+	return rows_out;
 }
 
 void free_row_crossings(row_crossings_t *rc, int num_rows) {
