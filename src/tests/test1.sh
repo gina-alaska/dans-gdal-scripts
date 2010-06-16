488fd0cd17366030f02e3fd00dce85517d0ae9f8
Dan Stahlke <dan@gina.alaska.edu>
2010-06-16 10:49:38 -0800

    updated test cases

--- src/tests/test1.sh
+++ src/tests/test1.sh
@@ -5,13 +5,13 @@
 #../gdal_trace_outline testcase4.png -ndv 255 -out-cs xy -wkt-out good-tc4.wkt -split-polys -dp-toler 0
 #../gdal_trace_outline testcase5.png -ndv 255 -out-cs xy -wkt-out good-tc5.wkt -split-polys -dp-toler 0
 rm -f test-tc*.wkt
-../gdal_trace_outline testcase.tif  -ndv 255 -out-cs xy -wkt-out test-tc1.wkt -split-polys -dp-toler 0
-../gdal_trace_outline testcase2.tif -ndv 255 -out-cs xy -wkt-out test-tc2.wkt -split-polys -dp-toler 0
-../gdal_trace_outline testcase3.tif -ndv 255 -out-cs xy -wkt-out test-tc3.wkt -split-polys -dp-toler 0
-../gdal_trace_outline testcase4.png -ndv 255 -out-cs xy -wkt-out test-tc4.wkt -split-polys -dp-toler 0
-../gdal_trace_outline testcase5.png -ndv 255 -out-cs xy -wkt-out test-tc5.wkt -split-polys -dp-toler 0
+../gdal_trace_outline testcase.tif  -ndv 255 -out-cs xy -wkt-out test-tc1.wkt -report test-tc1.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase2.tif -ndv 255 -out-cs xy -wkt-out test-tc2.wkt -report test-tc2.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase3.tif -ndv 255 -out-cs xy -wkt-out test-tc3.wkt -report test-tc3.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase4.png -ndv   0 -out-cs xy -wkt-out test-tc4.wkt -report test-tc4.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase5.png -ndv 255 -out-cs xy -wkt-out test-tc5.wkt -report test-tc5.ppm -split-polys -dp-toler 0
 
-../gdal_list_corners -inspect-rect4 -erosion -ndv 0 testcase4.png > test-tc4-rect.wkt
+../gdal_list_corners -inspect-rect4 -erosion -ndv 0 testcase4.png -report test-tc4-rect.ppm > test-tc4-rect.wkt
 
 #for i in 1 2 3 4 5 ; do md5sum good-tc$i.wkt test-tc$i.wkt ; done
 for i in 1 2 3 4 5 ; do 
