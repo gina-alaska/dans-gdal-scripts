1dc21ab7a8ddd864c2e2ca56cd9627b762896db2
Dan Stahlke <dan@gina.alaska.edu>
2010-06-17 15:48:10 -0800

    another test case

--- src/tests/test1.sh
+++ src/tests/test1.sh
@@ -7,11 +7,12 @@ rm -f out_*.wkt
 ../gdal_trace_outline testcase_5.png     -b 1 -ndv 255 -out-cs xy -wkt-out out_5.wkt     -report out_5.ppm     -split-polys -dp-toler 0
 ../gdal_trace_outline testcase_maze.png  -b 1 -ndv 255 -out-cs xy -wkt-out out_maze.wkt  -report out_maze.ppm  -split-polys -dp-toler 0
 ../gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise.wkt -report out_noise.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise_dp3.wkt -report out_noise_dp3.ppm -split-polys -dp-toler 3
 
 ../gdal_list_corners -inspect-rect4 -erosion -ndv 0 testcase_4.png -report out_4-rect.ppm > out_4-rect.wkt
 
 #for i in 1 2 3 4 5 ; do md5sum good-tc$i.wkt test-tc$i.wkt ; done
-for i in 1 2 3 4 5 maze noise ; do 
+for i in 1 2 3 4 5 maze noise noise_dp3 ; do 
 	if diff --brief good_$i.wkt out_$i.wkt ; then
 		echo "outline $i - Good"
 	else
