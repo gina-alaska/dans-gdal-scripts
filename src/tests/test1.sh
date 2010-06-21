fc9a8c883282419b4757fe527f0c91a50bc8aa0b
Dan Stahlke <dan@gina.alaska.edu>
2010-06-21 12:14:15 -0800

    more test cases

--- src/tests/test1.sh
+++ src/tests/test1.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 rm -f out_*.wkt
-../gdal_trace_outline testcase_1.tif     -ndv 255 -out-cs xy -wkt-out out_1.wkt     -report out_1.ppm     -split-polys -dp-toler 0
-../gdal_trace_outline testcase_2.tif     -ndv 255 -out-cs xy -wkt-out out_2.wkt     -report out_2.ppm     -split-polys -dp-toler 0
-../gdal_trace_outline testcase_3.tif     -ndv 255 -out-cs xy -wkt-out out_3.wkt     -report out_3.ppm     -split-polys -dp-toler 0
-../gdal_trace_outline testcase_4.png     -b 1 -ndv   0 -out-cs xy -wkt-out out_4.wkt     -report out_4.ppm     -split-polys -dp-toler 0
-../gdal_trace_outline testcase_5.png     -b 1 -ndv 255 -out-cs xy -wkt-out out_5.wkt     -report out_5.ppm     -split-polys -dp-toler 0
+../gdal_trace_outline testcase_1.tif      -ndv 255 -out-cs xy -wkt-out out_1.wkt    -report out_1.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase_1.tif      -ndv 255 -out-cs en -wkt-out out_1_en.wkt -dp-toler 0
+../gdal_trace_outline testcase_2.tif      -ndv 255 -out-cs xy -wkt-out out_2.wkt    -report out_2.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase_3.tif      -ndv 255 -out-cs xy -wkt-out out_3.wkt    -report out_3.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase_4.png -b 1 -ndv   0 -out-cs xy -wkt-out out_4.wkt    -report out_4.ppm -split-polys -dp-toler 0
+../gdal_trace_outline testcase_5.png -b 1 -ndv 255 -out-cs xy -wkt-out out_5.wkt    -report out_5.ppm -split-polys -dp-toler 0
 ../gdal_trace_outline testcase_maze.png  -b 1 -ndv 255 -out-cs xy -wkt-out out_maze.wkt  -report out_maze.ppm  -split-polys -dp-toler 0
 ../gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise.wkt -report out_noise.ppm -split-polys -dp-toler 0
 ../gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise_dp3.wkt -report out_noise_dp3.ppm -split-polys -dp-toler 3
 
 ../gdal_list_corners -inspect-rect4 -erosion -ndv 0 testcase_4.png -report out_4-rect.ppm > out_4-rect.wkt
 
+../gdal_wkt_to_mask -wkt good_1_en.wkt -geo-from testcase_1.tif -mask-out out_1_mask.ppm
+
 #for i in 1 2 3 4 5 ; do md5sum good-tc$i.wkt test-tc$i.wkt ; done
-for i in 1 2 3 4 5 maze noise noise_dp3 ; do 
+for i in 1 1_en 2 3 4 5 maze noise noise_dp3 ; do 
 	if diff --brief good_$i.wkt out_$i.wkt ; then
 		echo "outline $i - Good"
 	else
@@ -24,4 +27,10 @@ if diff --brief good_4-rect.wkt out_4-rect.wkt ; then
 	echo "rect - Good"
 else
 	echo "rect - Bad"
+fi
+
+if diff --brief good_1_mask.ppm out_1_mask.ppm ; then
+	echo "wkt2mask - Good"
+else
+	echo "wkt2mask - Bad"
 fi
