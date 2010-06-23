e6333358ee4d88618a4caf9e0cb27cce6d42c4aa
Dan Stahlke <dan@gina.alaska.edu>
2010-06-22 16:14:07 -0800

    changeable bindir

--- src/tests/test1.sh
+++ src/tests/test1.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 rm -f out_*
 
-../gdal_trace_outline testcase_1.tif -ndv 255 -out-cs xy -wkt-out out_1.wkt    -report out_1.ppm -split-polys -dp-toler 0
-../gdal_trace_outline testcase_1.tif -ndv 255 -out-cs en -wkt-out out_1_en.wkt -dp-toler 0
-../gdal_trace_outline testcase_2.tif -ndv 255 -out-cs xy -wkt-out out_2.wkt    -report out_2.ppm -split-polys -dp-toler 0
-../gdal_trace_outline testcase_3.tif -ndv 255 -out-cs xy -wkt-out out_3.wkt    -report out_3.ppm -split-polys -dp-toler 0
-../gdal_trace_outline testcase_4.png -ndv '0..255 0..255 0..255 0' -out-cs xy -wkt-out out_4.wkt    -report out_4.ppm -split-polys -dp-toler 0
-../gdal_trace_outline testcase_5.png -ndv 255 -out-cs xy -wkt-out out_5.wkt    -report out_5.ppm -split-polys -dp-toler 0
-../gdal_trace_outline testcase_maze.png  -ndv 255 -out-cs xy -wkt-out out_maze.wkt  -report out_maze.ppm  -split-polys -dp-toler 0
-../gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise.wkt -report out_noise.ppm -split-polys -dp-toler 0
-../gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise_dp3.wkt -report out_noise_dp3.ppm -split-polys -dp-toler 3
+BINDIR=..
 
-../gdal_trace_outline testcase_3.tif -out-cs xy -wkt-out out_3_classify.wkt -dp-toler 0 -classify
-../gdal_trace_outline pal.tif -out-cs xy -wkt-out out_3_classify_pal.wkt -dp-toler 0 -classify
+$BINDIR/gdal_trace_outline testcase_1.tif -ndv 255 -out-cs xy -wkt-out out_1.wkt    -report out_1.ppm -split-polys -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_1.tif -ndv 255 -out-cs en -wkt-out out_1_en.wkt -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_2.tif -ndv 255 -out-cs xy -wkt-out out_2.wkt    -report out_2.ppm -split-polys -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_3.tif -ndv 255 -out-cs xy -wkt-out out_3.wkt    -report out_3.ppm -split-polys -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_4.png -ndv '0..255 0..255 0..255 0' -out-cs xy -wkt-out out_4.wkt    -report out_4.ppm -split-polys -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_5.png -ndv 255 -out-cs xy -wkt-out out_5.wkt    -report out_5.ppm -split-polys -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_maze.png  -ndv 255 -out-cs xy -wkt-out out_maze.wkt  -report out_maze.ppm  -split-polys -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise.wkt -report out_noise.ppm -split-polys -dp-toler 0
+$BINDIR/gdal_trace_outline testcase_noise.png -b 1 -ndv   0 -out-cs xy -wkt-out out_noise_dp3.wkt -report out_noise_dp3.ppm -split-polys -dp-toler 3
 
-../gdal_list_corners -inspect-rect4 -erosion -ndv 0 testcase_4.png -report out_4-rect.ppm > out_4-rect.wkt
+$BINDIR/gdal_trace_outline testcase_3.tif -out-cs xy -wkt-out out_3_classify.wkt -dp-toler 0 -classify
+$BINDIR/gdal_trace_outline pal.tif -out-cs xy -wkt-out out_3_classify_pal.wkt -dp-toler 0 -classify
 
-../gdal_wkt_to_mask -wkt good_1_en.wkt -geo-from testcase_1.tif -mask-out out_1_mask.ppm
+$BINDIR/gdal_list_corners -inspect-rect4 -erosion -ndv 0 testcase_4.png -report out_4-rect.ppm > out_4-rect.wkt
 
-../gdal_get_projected_bounds -s_wkt good_1_en.wkt -s_srs '+proj=utm +zone=6 +ellps=WGS84 +units=m +no_defs ' -t_srs '+proj=stere +lat_ts=80 +lat_0=90 +lon_0=0 +ellps=WGS84' -report out_projbounds_report.ppm > out_projbounds.yml
+$BINDIR/gdal_wkt_to_mask -wkt good_1_en.wkt -geo-from testcase_1.tif -mask-out out_1_mask.ppm
+
+$BINDIR/gdal_get_projected_bounds -s_wkt good_1_en.wkt -s_srs '+proj=utm +zone=6 +ellps=WGS84 +units=m +no_defs ' -t_srs '+proj=stere +lat_ts=80 +lat_0=90 +lon_0=0 +ellps=WGS84' -report out_projbounds_report.ppm > out_projbounds.yml
 
 echo '####################'
 
--- src/tests/test3.sh
+++ src/tests/test3.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 rm -f out_*
 
-../gdal_dem2rgb nedcut.tif out_dem.tif -default-palette && tifftopnm out_dem.tif >out_dem.pnm
-../gdal_contrast_stretch -ndv '0..255 0..255 0..255 0' -histeq 50 testcase_4.png out_histeq.tif && tifftopnm out_histeq.tif >out_histeq.pnm
+BINDIR=..
+
+$BINDIR/gdal_dem2rgb nedcut.tif out_dem.tif -default-palette && tifftopnm out_dem.tif >out_dem.pnm
+$BINDIR/gdal_contrast_stretch -ndv '0..255 0..255 0..255 0' -histeq 50 testcase_4.png out_histeq.tif && tifftopnm out_histeq.tif >out_histeq.pnm
 
 echo '####################'
 
